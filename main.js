cdd6b1b70ba386ace1bc7630fa2e51001eb2e6f7
Kiryl1337 <kiryl.kniazeu@gmail.com>
2022-03-28 15:45:59 +0300

    feat: add skills toggle with js

---
+++ main.js
@@ -0,0 +1,7 @@
+const skillItems = document.querySelectorAll('section.skills .skill')
+
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
+        skill.querySelector('.items').classList.toggle('show-items')
+    })
+})
