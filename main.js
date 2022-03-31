1be5d0b6e9b5248750a596c0a477a21ed83ee143
Kiryl1337 <kiryl.kniazeu@gmail.com>
2022-03-31 17:52:12 +0300

    feat: customizing scroll and adding box-shadow using js

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ skillItems.forEach(skill => {
         skill.querySelector('.items').classList.toggle('show-items')
     })
 })
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
+})
